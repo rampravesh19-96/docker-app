65a915381cb734d65cbedbec8bddcf2f40ac9b1c
ram <rampraveshsingh1996@gmail.com>
2026-01-03 10:33:13 +0000

    testing

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = 3000;
 app.get('/', (req, res) => {
 res.send('PM2 app is running');
 });
+app.get("/test",(req,res)=>{
+	res.send("testing auto deploy")
+})
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
